a654b4ed894f744a979c9eb48c9dfc1f1dc38e45
damiano.barbati <damiano.barbati@gmail.com>
2022-09-21 09:39:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# playwright-bug repro
+
+Run with:
+```sh
+nvm install
+yarn install
+yarn test:e2e
+```
---
+++ playwright.config.ts
@@ -0,0 +1,48 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+// ref: https://playwright.dev/docs/test-configuration
+const config: PlaywrightTestConfig = {
+  timeout: 30 * 1000, // timeout per test
+  testDir: './e2e', // test directory
+  retries: 2, // if a test fails, retry it additional 2 times
+  outputDir: 'test-results/', // artifacts folder where screenshots, videos, and traces are stored.
+
+  // ref: https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  webServer: {
+    command: 'yarn dev',
+    url: `http://localhost`,
+    timeout: 30 * 1000,
+    reuseExistingServer: !!process.env.CI,
+  },
+
+  use: {
+    baseURL: `http://localhost`,
+    headless: true,
+
+    // ref: https://playwright.dev/docs/trace-viewer
+    trace: 'retry-with-trace',
+
+    // ref: https://playwright.dev/docs/api/class-browser#browser-new-context
+    contextOptions: {
+      ignoreHTTPSErrors: true,
+    },
+  },
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: devices['Desktop Chrome'],
+    },
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5'],
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+};
+
+export default config;
