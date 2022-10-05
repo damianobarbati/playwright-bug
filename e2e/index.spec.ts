ce8b9f28b69e83f5a07a0713fbe8962e6eceee4d
damiano.barbati <damiano.barbati@gmail.com>
2022-10-05 10:25:34 +0200

    fix spec

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('https://google.com');
-  await expect(page).toHaveURL('/');
+  await expect(page).toHaveURL(/\/$/);
 });
